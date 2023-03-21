ed43c4dd6f3085cb9720a3cc6b7f663b4c5ffa4b
neoosio <neo.osio@hotmail.com>
2023-03-21 11:20:00 -0300

    creando mi primer repositor

---
+++ script.js
@@ -0,0 +1,74 @@
+/* let nombre = "pepe"
+if(true){
+    let nombre = "lucas"
+}
+console.log(nombre)
+console.log(nombre) */
+
+
+//Mostrar por consola del 1 al 100 todos los numeros pares
+
+
+
+/* const averiguarPares = (inicio, final) =>{
+    for(let i = inicio; i <= final; i++){
+        if(i % 2 == 0){
+            console.log(i + " es impar")
+        }else{
+            console.log(i + " es impar")
+        }
+    }
+}
+
+averiguarPares(77, 99) */
+
+
+/* let contador = 0 */
+
+/* contador++
+contador++
+console.log(++contador) */
+
+/* contador += 5
+contador += 5 */
+
+const nombres = ["pepe", "maria", "lucas", "juan"]
+
+/* for(let i = 0; i < nombres.length; i++){
+    let nombre = nombres[i]
+    console.log(nombre)
+} */
+
+//FOR..OF
+/* for(let nombre of nombres){
+    console.log(nombre)
+} */
+
+/* const ganancias = [ 213,212132,456773,67575, 6545] */
+
+//Recorrer el array y calcular la ganancia total
+
+/* let gananciaTotal = 0
+
+for(let ganancia of ganancias){
+    gananciaTotal += ganancia
+}
+
+console.log("La ganancia total del dia es: " + gananciaTotal) */
+
+let dinero = 5
+let tengoSueno = true
+//voy a comprar si tengo mas de 10 pesos y no tengo sueño
+/* if(dinero > 10 && !tengoSueno){
+    console.log("voy a comprar")
+}else{
+    console.log("No voy a comprar")
+} */
+
+//voy a comprar si tengo mas de 10 pesos o no tengo sueño
+
+/* if(dinero > 10 || !tengoSueno){
+    console.log("voy a comprar")
+}else{
+    console.log("No voy a comprar")
+}  */
